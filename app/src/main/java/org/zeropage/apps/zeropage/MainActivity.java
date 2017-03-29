49458abcfe29dc94994d46fef8a3989fafba9469
hansung <gnidoc327@gmail.com>
2017-03-29 17:45:56 +0900

    Revert "gitignore roll back"
    This reverts commit 2fe32be486fdaf1d71ec6430031929dd309ef302.

--- app/src/main/java/org/zeropage/apps/zeropage/MainActivity.java
+++ app/src/main/java/org/zeropage/apps/zeropage/MainActivity.java
@@ -1,9 +1,7 @@
-package org.zeropage.apps.zeropage.main;
+package org.zeropage.apps.zeropage;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import org.zeropage.apps.zeropage.R;
 
 public class MainActivity extends AppCompatActivity {
 
