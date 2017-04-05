53a412a7f3a5f3b261501bca6155453c6b805247
nErumin <nerumin90@gmail.com>
2017-04-06 01:20:55 +0900

    add Authenticate cloud function to the Firebase.
    However, it isn't available now because of pricing.

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,8 +1,41 @@
-var functions = require('firebase-functions');
+var functions = require('firebase-functions')
+var admin = require('firebase-admin')
+var request = require('request')
+var express = require('express')
+var bodyParser = require('body-parser')
+var requestPromise = require('request-promise')
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// })
+var expressApp = express()
+
+expressApp.use(bodyParser.json())
+expressApp.use(bodyParser.urlencoded({ extended: true }))
+
+exports.Authenticate = functions.https.onRequest(function(appRequest, appResponse) {
+    var apiUri = 'http://zerobot.herokuapp.com/hubot/otp/' + appRequest.body.username
+
+    var options = {
+        method: 'POST',
+        uri: apiUri,
+        body: { 
+            token: appRequest.body.token
+        },
+        json: true,
+        resolveWithFullResponse: true,
+        simple: false
+    };
+
+    console.log('Request to "' + apiUri 
+        + '" with token = "' + appRequest.body.token + '", user = "' + appRequest.body.username +'"');
+
+    requestPromise(options)
+        .then(function (apiResponse) {
+            console.log('Response code = ' + apiResponse.statusCode);
+            console.log('The API authentication process was successful? = ' + (apiResponse.statusCode == 200));
+            appResponse.status(apiResponse.statusCode).end();
+        })
+        .catch(function (error) {
+            console.log('Error happened in the API request.');
+            console.log(error);
+            appResponse.status(400).end();
+        })
+})
