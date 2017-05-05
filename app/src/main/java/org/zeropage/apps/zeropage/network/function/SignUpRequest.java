9e3b42064c985a40cb48ad20ba6d6e60fae00c1a
nErumin <nerumin90@gmail.com>
2017-05-06 02:45:31 +0900

    add a additional request parameter in SignUpRequest.
    The added parameter is for sending a FCMToken to cloud functions.

--- app/src/main/java/org/zeropage/apps/zeropage/network/function/SignUpRequest.java
+++ app/src/main/java/org/zeropage/apps/zeropage/network/function/SignUpRequest.java
@@ -3,6 +3,8 @@ package org.zeropage.apps.zeropage.network.function;
 
 import android.support.annotation.NonNull;
 
+import com.annimon.stream.function.FunctionalInterface;
+
 import org.zeropage.apps.zeropage.network.common.RequestParameter;
 
 import retrofit2.Call;
@@ -10,10 +12,12 @@ import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
 
+@FunctionalInterface
 public interface SignUpRequest {
     String BASE_URL = "https://us-central1-zapp-c562d.cloudfunctions.net/";
 
     @POST(FunctionType.SIGN_UP)
     @FormUrlEncoded
-    Call<String> execute(@Field(RequestParameter.USERNAME) @NonNull String username);
+    Call<String> execute(@Field(RequestParameter.USERNAME) @NonNull String username,
+                         @Field(RequestParameter.TOKEN) @NonNull String fcmToken);
 }
