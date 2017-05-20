47b7b129bfa9f7a48bada8de9033b01a9ced74dc
hyomNam <skagbghk@gmail.com>
2017-05-20 17:02:47 +0900

    Edit MainActivity for BackPressCloseHandler Test

--- app/src/main/java/org/zeropage/apps/zeropage/main/MainActivity.java
+++ app/src/main/java/org/zeropage/apps/zeropage/main/MainActivity.java
@@ -7,9 +7,21 @@ import org.zeropage.apps.zeropage.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private BackPressCloseHandler backPressCloseHandler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        backPressCloseHandler = new BackPressCloseHandler(this);
     }
+
+    @Override
+    public void onBackPressed() {
+        backPressCloseHandler.onBackPressed();
+    }
+
+
+
+
 }
