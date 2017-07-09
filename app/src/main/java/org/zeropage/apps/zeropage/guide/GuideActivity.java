7157d4162b0a4e5f0d2bd750f2b5e024232322a2
uoyssim <simjaecheol@naver.com>
2017-07-09 15:40:15 +0900

    Glide 적용

--- app/src/main/java/org/zeropage/apps/zeropage/guide/GuideActivity.java
+++ app/src/main/java/org/zeropage/apps/zeropage/guide/GuideActivity.java
@@ -1,17 +1,23 @@
 package org.zeropage.apps.zeropage.guide;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.melnykov.fab.FloatingActionButton;
 
 import org.zeropage.apps.zeropage.R;
+import org.zeropage.apps.zeropage.login.LoginActivity;
 
 /**
  * Created by gnidoc on 2017-03-29.
  */
 public class GuideActivity extends AppCompatActivity {
     ViewPager viewPager;
-
+    FloatingActionButton fab;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +26,13 @@ public class GuideActivity extends AppCompatActivity {
         viewPager=(ViewPager)findViewById(R.id.view_pager);
         GuideAdapter guideAdapter = new GuideAdapter(getSupportFragmentManager());
         viewPager.setAdapter(guideAdapter);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+               Intent intent = new Intent(GuideActivity.this , LoginActivity.class);
+               startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/org/zeropage/apps/zeropage/guide/Page.java
+++ app/src/main/java/org/zeropage/apps/zeropage/guide/Page.java
@@ -1,32 +1,43 @@
 package org.zeropage.apps.zeropage.guide;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.melnykov.fab.FloatingActionButton;
 
 import org.zeropage.apps.zeropage.R;
+import org.zeropage.apps.zeropage.login.LoginActivity;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class Page extends Fragment {
-    int page;
-
+    int page, img;
+    FloatingActionButton fab;
     public Page() {
 
     }
-    public void setPage(int page){
+    public void setPage(int page) {
         this.page = page;
     }
-
+    public void setImg(int img){
+        this.img = img;
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(page, container, false);
+        View view = inflater.inflate(page, container, false);
+        ImageView iv = (ImageView)view.findViewById(R.id.ImageView);
+        Glide.with(this).load(img).into(iv);
+        return view;
     }
 
 }
